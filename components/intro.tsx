731c87f8f7d1c6af6129bbaffcea81f2da0ef6e6
Mangesh Ahirrao <mahirrao925@gmail.com>
2024-02-09 00:02:17 -0600

    third commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -101,7 +101,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Mangesh_Resume_Fall2023.pdf"
+          href="/Mangesh_Resume_2024.pdf"
           download
         >
           Download CV{" "}
